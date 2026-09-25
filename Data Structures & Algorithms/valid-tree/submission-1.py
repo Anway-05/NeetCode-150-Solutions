fd96417ffbe1d18950422e298b06a0d297825977
Anway-05 <anwaymukherjee77@gmail.com>
2026-09-26 01:02:55 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph=[[] for _ in range(n)]
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        dq=deque([(0,-1)])
+        visited={0}
+        while dq:
+            node,parent=dq.popleft()
+            for neighbor in graph[node]:
+                if neighbor==parent:
+                    continue
+                if neighbor in visited:
+                    return False
+                visited.add(neighbor)
+                dq.append((neighbor,node))
+        if len(visited)==n:
+            return True
+        else:
+            return False
+        
